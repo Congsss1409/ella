348950a89d88f257ae720796f3ec269bf6f0fd84
Congsss1409 <marcotaba1121@gmail.com>
2024-05-03 22:52:08 +0800

    added modal to view

--- admin/index.php
+++ admin/index.php
@@ -70,7 +70,7 @@
 
         <!-- Modal for adding employee -->
         <div class="modal fade" id="addEmployeeModal" tabindex="-1" aria-labelledby="addEmployeeModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="addEmployeeModalLabel">Add Employee</h5>
@@ -90,6 +90,20 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="viewEmployeeModal" tabindex="-1" aria-labelledby="viewEmployeeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="viewEmployeeModalLabel">Employee Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="employeeDetails">
+                <!-- Employee details will be loaded here using AJAX -->
+            </div>
+        </div>
+    </div>
+</div>
 
     <div class="container">
         <a href="../index.php" class="btn btn-danger mb-3">Log Out</a>
@@ -114,7 +128,8 @@
                     echo "<tr>";
                     echo "<td>".$row['emp_id']."</td>";
                     echo "<td>".$row['emp_name']."</td>";
-                    echo "<td><a href='view_details.php?id=".$row['emp_id']."' class='btn btn-info btn-sm'>View Details</a> ";
+                    echo "<td><button type='button' class='btn btn-info btn-sm view-details-btn' data-bs-toggle='modal' data-bs-target='#viewEmployeeModal' data-id='".$row['emp_id']."'>View Details</button> ";
+
                     echo "<a href='add_incentive.php?id=".$row['emp_id']."' class='btn btn-success btn-sm'>Add Incentive</a> ";
                     echo "<a href='delete_employee.php?id=".$row['emp_id']."' class='btn btn-danger btn-sm'>Delete</a></td>";
                     echo "</tr>";
@@ -128,7 +143,24 @@
             mysqli_close($conn);
         ?>
     </div>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="bootstrap.bundle.min.js"></script>
+    <script>
+    // JavaScript to load employee details dynamically when modal is shown
+    $(document).on('click', '.view-details-btn', function() {
+        var empId = $(this).data('id'); // Extract employee ID from data-id attribute
+        var modal = $('#viewEmployeeModal');
+        // AJAX request to fetch employee details
+        $.ajax({
+            type: 'GET',
+            url: 'get_employee_details.php?id=' + empId,
+            success: function(response) {
+                modal.find('.modal-body').html(response); // Inject employee details into modal body
+            }
+        });
+    });
+</script>
+
+    
 </body>
 </html>
